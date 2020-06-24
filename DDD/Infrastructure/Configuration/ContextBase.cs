db38f42e2f7db9fab63dcaac1d28ab70a4c6aaca
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2020-06-23 22:15:01 -0300

    Infra

---
+++ DDD/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                // para o UseSqlServer foi instalado o pacto Microsoft.EntityFrameworkCore.SqlServer
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=DDD;Integrated Security=False;User ID=sa;Password=sa;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            return strCon;
+        }
+    }
+}
---
+++ DDD/Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,69 @@
+﻿using Domains.Interfaces.Generics;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        public Task Add(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+    }
+}
